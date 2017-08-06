4cadc9357131941a803331f9e04d36126e2ca82b
Yuma Okabe <rozabmmsr@gmail.com>
2017-08-06 15:38:43 +0000

    Set file type and size limit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,8 +5,17 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   has_attached_file :image, styles: { medium: "200x150>", thumb: "50x50>" }
-  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
 
+  #拡張子の制限
+  validates_attachment_content_type :image,
+    :content_type => ['image/jpg', 'image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png'],
+    :message => "JPG, GIF, PNGのみアップロードできます"
+    
+  #ファイルサイズの制限
+  validates_attachment_size :image,
+    :less_than => 2.megabytes,
+    :message => "ファイルサイズが大きすぎます(最大2MBまで)"
+    
   def self.from_users_followed_by(user)
     followed_user_ids = "SELECT followed_id FROM relationships
                          WHERE follower_id = :user_id"
